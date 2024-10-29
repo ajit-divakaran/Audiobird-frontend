768dfdf0e982ee28dce2a401069c279f0d7cd8a4
SpYrO272 <princeaj272@gmail.com>
2024-10-29 19:52:50 +0530

    change

--- src/services/allApi.js
+++ src/services/allApi.js
@@ -5,3 +5,12 @@ import {serverurl} from "./serverurl"
  export const AddCategoryApi = async(reqbody)=>{
     return await commonApi('POST',`${serverurl}/category`,reqbody)
  }
+
+ //add 
+export const AddBookApi = async(reqBody)=>{
+   return await commonApi('POST',`${serverurl}/playlist`,reqBody)
+}
+//delete
+export const DeleteBookApi = async(id)=>{
+   return await commonApi('DELETE', `${serverurl}/playlist/${id}`)
+}
