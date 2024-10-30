666215ac40eb79580964a610b74b74ffe53f4888
SpYrO272 <princeaj272@gmail.com>
2024-10-30 12:07:04 +0530

    updated

--- src/services/allApi.js
+++ src/services/allApi.js
@@ -5,3 +5,19 @@ import {serverurl} from "./serverurl"
  export const AddCategoryApi = async(reqbody)=>{
     return await commonApi('POST',`${serverurl}/category`,reqbody)
  }
+
+  //add audiobook history
+
+  export const addAudiobookHistoryApi = async(reqBody)=>{
+   return await commonApi('POST',`${serverurl}/history`,reqBody)
+}
+
+// get audiobook from history
+export const getAudiobookHistoryApi = async()=>{
+   return await commonApi('GET',`${serverurl}/history`)
+}
+
+//delete audiobook from history
+export const deleteHistoryAudiobookApi = async(id)=>{
+   return await commonApi('DELETE',`${serverurl}/history/${id}`)
+} 
